6c4bf939419842c63340c23939b2dc708a75fdaf
isa <ishafajar11@gmail.com>
2019-11-12 13:53:05 +0700

    Initial Commit

---
+++ ModelViewViewModel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ModelViewViewModel
+//
+//  Created by isa nur fajar on 12/11/19.
+//  Copyright © 2019 isa nur fajar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
